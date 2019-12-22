4bb2f37d4f8a9e6c18c5bacf6667652a8cfb20b1
Trường Nguyễn <truongnexo1011@hotmail.com>
2019-12-22 21:45:38 +0700

    Add Enemy, gameWin, Lose

--- Assets/Script/DrBlackFriday.cs
+++ Assets/Script/DrBlackFriday.cs
@@ -8,8 +8,7 @@ public class DrBlackFriday : Enemy
     private Animator anim;
 
     public void Start()
-    {
-        // khoi tao doi tuong
+    { 
         r2D = gameObject.GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
@@ -17,21 +16,18 @@ public class DrBlackFriday : Enemy
     public override void FixedUpdate()
     {
         Vector3 temp = transform.localScale;
-        speed = 0;
-        //anim.SetBool("Hit", false);
-        anim.SetBool("Walk", false);
-        // lay khoang cach tu skeleton toi player
+        speed = 0; 
+        anim.SetBool("Walk", false); 
         distance = Vector2.Distance(transform.position, player.position);
 
         // cho quai thuc day khi player nam trong khoang distanceMIN va ngu lai khi player ra khoi
         if (player.transform.position.x > this.transform.position.x && distance < distanceMIN)
         {
-            speed = 0.2f;
+            speed = 10f;
             anim.SetBool("Walk", true);
             temp.x = 3f;
-            transform.localScale = temp;
-            // neu player den qua gan thi Attack
-            if (distance <= 1)
+            transform.localScale = temp; 
+            if (distance <= 100)
             {
                 anim.SetBool("Attack", true);
                 speed = 0;
@@ -39,19 +35,19 @@ public class DrBlackFriday : Enemy
             else
             {
                 anim.SetBool("Attack", false);
-                speed = 0.2f;
+                speed = 1.2f;
             }
         }
         else
 
         if (player.transform.position.x < this.transform.position.x && distance < distanceMIN)
         {
-            speed = 0.2f;
+            speed = 10f;
             r2D.AddForce(Vector2.left * speed);
             anim.SetBool("Walk", true);
             temp.x = -3f;
             transform.localScale = temp;
-            if (distance < 1)
+            if (distance < 100)
             {
                 anim.SetBool("Attack", true);
                 speed = 0;
@@ -60,7 +56,7 @@ public class DrBlackFriday : Enemy
             else
             {
                 anim.SetBool("Attack", false);
-                speed = 0.2f;
+                speed = -1.2f;
 
             }
         }
@@ -81,20 +77,17 @@ public class DrBlackFriday : Enemy
 
     }
 
-    //kiem tra xem player co chem chung khong
+    //kiem tra xem player co chem trung khong
     void Check_Sword_Arc()
     {
-        float checkSword = Vector2.Distance(Sword_Arc.position, transform.position); // khoang cach giua vet chem voi enemy
-        //Debug.Log("distanceSword" + checkSword);
-        if (checkSword < 1 && Input.GetMouseButtonDown(0))
+        float checkSword = Vector2.Distance(Sword_Arc.position, transform.position); 
+        if (checkSword < 150 && Input.GetKeyDown(KeyCode.Space))
         {
             health -= 10;
-            //anim.SetBool("Hit", true);
             anim.SetTrigger("Hit");
             r2D.AddForce(Vector2.right * speed * 0);
         }
     }
-    // Update is called once per frame
     void Update()
     {
         
